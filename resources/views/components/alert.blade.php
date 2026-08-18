0897c4d7aa20f632783b07e4df2cc85794b292f9
Vincent Genesius <vincentgtkj@gmail.com>
2026-08-18 07:52:30 +0700

    on progress

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,4 +1,4 @@
 <div class="border border-red-500 bg-red-100 rounded-lg p-4">
     <h1 class="text-lg text-red-500 font-bold">Error</h1>
-    <p class="text-red-500">Terdapat kesalahan ketika menambahkan data siswa baru ke dalam sistem</p>
+    <p class="text-red-500">{{ $slot }}</p>
 </div>
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,6 +3,10 @@
 @section('title', $title)
 
 @section('content')
+
+    <x-alert>
+        Terdapat kesalahan ketika menambahkan data siswa baru ke dalam sistem sekolah
+    </x-alert>
     <div class="mb-8 flex items-end justify-between border-b border-[#E5E3DB] pb-5">
 
         <div>
